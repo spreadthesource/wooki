5097f1e27de436f601246c793e5406a53a8daaf3
Robin Komiwes <odisseus@gmail.com>
2009-11-17 13:56:47 +0100

    added active bubble for already commented blocks

--- src/main/webapp/static/js/wooki.js
+++ src/main/webapp/static/js/wooki.js
@@ -4,19 +4,21 @@ jQuery.extend(Wooki, {
 	bubbles :  {
 		init : function() {
 			jQuery('.commentable').each(function(i) {
-			jQuery(this).bind("mouseenter mouseleave", function(e){
-				jQuery(".comment-accessor .no-comment").css('visibility','hidden');
-						
+				
 				comment = jQuery('#' +jQuery(this).attr('id').replace('b','c'));
 				comment.css({
 					'top': (jQuery(this).offset().top + 10) + 'px',
 					'left': (jQuery(this).offset().left - 50)  + 'px',
 					'height' : jQuery(this).height() + 'px',
 				});
-			    comment.css('visibility','visible');
-			    
-			    jQuery('#' +jQuery(this).attr('id').replace('b','c') + ' .no-comment').css('visibility', 'visible');
-			 });
+				
+				comment.css('visibility','visible');
+				
+				jQuery(this).bind("mouseenter mouseleave", function(e){
+					jQuery(".comment-accessor .no-comment").css('visibility','hidden');
+							    
+				    jQuery('#' +jQuery(this).attr('id').replace('b','c') + ' .no-comment').css('visibility', 'visible');
+				});
 			});
 			
 			jQuery('#book').bind("mouseleave", function(e){

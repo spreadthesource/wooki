73653856dda6fd0057678bd0f94eb9df82f5d029
Cordenier <christophe.cordenier@gmail.com>
2009-11-21 12:48:02 +0100

    Implement spring security authentication with signin and logout now running

---
+++ src/main/java/com/wooki/base/WookiBase.java
@@ -0,0 +1,18 @@
+package com.wooki.base;
+
+import org.apache.tapestry5.annotations.PageAttached;
+import org.apache.tapestry5.annotations.Property;
+import org.springframework.security.context.SecurityContextHolder;
+
+public class WookiBase {
+
+	@Property
+	private String username;
+
+	@PageAttached
+	private void setupUsername() {
+		username = SecurityContextHolder.getContext()
+				.getAuthentication().getName();
+	}
+
+}

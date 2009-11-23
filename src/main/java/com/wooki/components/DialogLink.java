935242fa642f45b671a579bbf52cf6aa2c209241
Robin Komiwes <odisseus@gmail.com>
2009-11-23 13:18:08 +0100

    renamed book/Index to book/IndexDesign as a temporary draft for implementing UI

---
+++ src/main/java/com/wooki/components/DialogLink.java
@@ -0,0 +1,42 @@
+package com.wooki.components;
+
+import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ClientElement;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.RenderSupport;
+import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.BeginRender;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+public class DialogLink implements ClientElement {
+	@Parameter(defaultPrefix = BindingConstants.LITERAL)
+	private String clientId;
+
+	@Parameter(required = true)
+	private String dialogId;
+
+	@Inject
+	private RenderSupport support;
+
+	@Inject
+	private ComponentResources resources;
+
+	@BeginRender
+	void startDiv(MarkupWriter writer) {
+		writer.element("a", "id", getClientId(), "href", "#");
+	}
+
+	@AfterRender
+	void declareDialog(MarkupWriter writer) {
+		writer.end();
+		support.addScript("$j('#%s').", resources.getId());// TODO:
+	}
+
+	public String getClientId() {
+		if (clientId != null)
+			return support.allocateClientId(clientId);
+		return support.allocateClientId(resources);
+	}
+}

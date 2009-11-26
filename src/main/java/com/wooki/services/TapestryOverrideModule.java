78b9f731e9b7be4f6b1b2c9602eb4ccb4e34b709
Cordenier <christophe.cordenier@gmail.com>
2009-11-27 00:38:24 +0100

    Extend Tapestry Default Exception Request Handler to allow to specify multiple exception in function of the Exception. The implementation is a first shot and should evolve to allow redirection for exemple

---
+++ src/main/java/com/wooki/services/TapestryOverrideModule.java
@@ -0,0 +1,37 @@
+package com.wooki.services;
+
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.InjectService;
+import org.apache.tapestry5.services.AliasContribution;
+import org.apache.tapestry5.services.RequestExceptionHandler;
+
+
+/**
+ * Tapestry module use for tapestry overrides or customization.
+ *
+ * @author ccordenier
+ *
+ */
+public class TapestryOverrideModule {
+
+	public static void bind(ServiceBinder binder) {
+		binder.bind(RequestExceptionHandler.class,
+				WookiRequestExceptionHandler.class).withId(
+				"WookiRequestExceptionHandler");
+	}
+
+	/**
+	 * Alias default request handler.
+	 * 
+	 * @param exceptionHandler
+	 * @param configuration
+	 */
+	public static void contributeAlias(
+			@InjectService("WookiRequestExceptionHandler") RequestExceptionHandler exceptionHandler,
+			Configuration<AliasContribution> configuration) {
+		configuration.add(AliasContribution.create(
+				RequestExceptionHandler.class, exceptionHandler));
+	}
+
+}

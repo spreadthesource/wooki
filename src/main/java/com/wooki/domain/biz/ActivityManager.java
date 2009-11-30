91ae4b1ddc6a22e1242d8eba8f61a40dbd3a3885
Cordenier <christophe.cordenier@gmail.com>
2009-11-30 23:50:23 +0100

    Create a package for business managers, not really sexy but if Wooki wants to expose its domain model in other ways than HTML, i hope it will be helpful

---
+++ src/main/java/com/wooki/domain/biz/ActivityManager.java
@@ -0,0 +1,20 @@
+package com.wooki.domain.biz;
+
+import java.util.List;
+
+import com.wooki.domain.model.Activity;
+import com.wooki.domain.model.FreshStuff;
+
+/**
+ * Use to handle activities on wikies element to follow history.
+ * 
+ * @author ccordenier
+ * 
+ */
+public interface ActivityManager {
+
+	List<Activity> listAll(int nbElts);
+
+	List<FreshStuff> listFreshStuff(int nbElts);
+
+}
---
+++ src/main/java/com/wooki/domain/biz/UserManagerImpl.java
@@ -0,0 +1,38 @@
+package com.wooki.domain.biz;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.providers.encoding.PasswordEncoder;
+import org.springframework.security.providers.encoding.ShaPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.wooki.domain.dao.UserDAO;
+import com.wooki.domain.exception.UserAlreadyException;
+import com.wooki.domain.model.User;
+import com.wooki.services.WookiModule;
+
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+@Component("userManager")
+public class UserManagerImpl implements UserManager {
+
+	@Autowired
+	private UserDAO authorDao;
+
+	@Transactional(readOnly = false, rollbackFor = UserAlreadyException.class)
+	public void addUser(User author) throws UserAlreadyException {
+		if (findByUsername(author.getUsername()) != null) {
+			throw new UserAlreadyException();
+		}
+		// Encode password into database
+		PasswordEncoder encoder = new ShaPasswordEncoder();
+		String pass = author.getPassword();
+		author.setPassword(encoder.encodePassword(pass, WookiModule.SALT));
+		authorDao.create(author);
+	}
+
+	public User findByUsername(String username) {
+		return authorDao.findByUsername(username);
+	}
+
+}

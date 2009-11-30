5b6b56f816002c7ca816af83f7a749a6fb284f8d
Cordenier <christophe.cordenier@gmail.com>
2009-11-30 23:51:54 +0100

    Create internal package for more Tapestry technical stuff

---
+++ src/main/java/com/wooki/services/internal/TapestryOverrideModule.java
@@ -0,0 +1,39 @@
+package com.wooki.services.internal;
+
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.InjectService;
+import org.apache.tapestry5.services.AliasContribution;
+import org.apache.tapestry5.services.RequestExceptionHandler;
+
+import com.wooki.services.WookiRequestExceptionHandler;
+
+
+/**
+ * Tapestry module use for tapestry overrides or customization.
+ *
+ * @author ccordenier
+ *
+ */
+public class TapestryOverrideModule {
+
+	public static void bind(ServiceBinder binder) {
+		binder.bind(RequestExceptionHandler.class,
+				WookiRequestExceptionHandler.class).withId(
+				"WookiRequestExceptionHandler");
+	}
+
+	/**
+	 * Alias default request handler.
+	 * 
+	 * @param exceptionHandler
+	 * @param configuration
+	 */
+	public static void contributeAlias(
+			@InjectService("WookiRequestExceptionHandler") RequestExceptionHandler exceptionHandler,
+			Configuration<AliasContribution> configuration) {
+		configuration.add(AliasContribution.create(
+				RequestExceptionHandler.class, exceptionHandler));
+	}
+
+}

c6f1e92582245b5c883fbaf5e95a234efc1b007e
Cordenier <christophe.cordenier@gmail.com>
2009-12-01 00:44:19 +0100

    Refactor again sorry :)

---
+++ src/main/java/com/wooki/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.wooki.services;
+
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.springframework.context.ApplicationContext;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.userdetails.UserDetails;
+import org.springframework.security.userdetails.UserDetailsService;
+import org.springframework.security.userdetails.UsernameNotFoundException;
+
+import com.wooki.domain.biz.UserManager;
+import com.wooki.domain.model.User;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Inject
+	private ApplicationContext applicationContext;
+	
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException, DataAccessException {
+		UserManager authManager = (UserManager) applicationContext.getBean("userManager");
+		User user = authManager.findByUsername(username);
+		return user;
+	}
+
+}

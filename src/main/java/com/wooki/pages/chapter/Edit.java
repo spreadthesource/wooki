113e72ae6d8f9dea4db7aaf1e97777be82aa630c
Cordenier <christophe.cordenier@gmail.com>
2009-12-01 00:46:07 +0100

    (Temporary) Fix Safari wymeditor crash

--- src/main/java/com/wooki/pages/chapter/Edit.java
+++ src/main/java/com/wooki/pages/chapter/Edit.java
@@ -26,7 +26,7 @@ public class Edit {
 	private Long bookId;
 
 	private Long chapterId;
-	
+
 	@Property
 	private String data;
 
@@ -36,6 +36,10 @@ public class Edit {
 	public void onActivate(Long bookId, Long chapterId) {
 		this.bookId = bookId;
 		this.chapterId = chapterId;
+	}
+
+	@OnEvent(value = EventConstants.PREPARE_FOR_RENDER)
+	public void prepareFormData() {
 		this.data = chapterManager.getContent(chapterId);
 	}
 

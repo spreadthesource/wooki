8b42224913138ba45f7d2c159d46155f440f05c1
Cordenier <christophe.cordenier@gmail.com>
2009-12-08 13:06:53 +0100

    Add print feature in book page, based on Bruno contribution

---
+++ src/main/java/com/wooki/services/ExportServiceImpl.java
@@ -0,0 +1,103 @@
+package com.wooki.services;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+
+import com.wooki.domain.biz.BookManager;
+import com.wooki.domain.biz.ChapterManager;
+import com.wooki.domain.model.Book;
+import com.wooki.domain.model.Chapter;
+import com.wooki.services.parsers.Convertor;
+
+public class ExportServiceImpl implements ExportService {
+
+	private BookManager bookManager;
+
+	private ChapterManager chapterManager;
+
+	private Convertor toFOConvertor;
+
+	private Convertor toPDFConvertor;
+
+	private Convertor toXHTMLConvertor;
+
+	public InputStream exportPdf(Long bookId) {
+		if (bookId == null) {
+			throw new IllegalArgumentException(
+					"Book id cannot be null to export.");
+		}
+
+		/** Generate XHTML from book */
+		Book b = bookManager.findById(bookId);
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("<html><head><title>").append(b.getTitle()).append(
+				"</title></head>");
+		buffer.append("<body>");
+		List<Chapter> chapters = chapterManager.listChapters(bookId);
+		if (chapters != null) {
+			for (Chapter c : chapters) {
+				buffer.append("<h1>").append(c.getTitle()).append("</h1>");
+				String content = chapterManager.getLastPublishedContent(c
+						.getId());
+				if (content != null) {
+					buffer.append(content);
+				}
+			}
+		}
+		buffer.append("</body></html>");
+
+		/** Generate PDF */
+		Resource resource = new ByteArrayResource(buffer.toString().getBytes());
+		InputStream xhtml = toXHTMLConvertor.performTransformation(resource);
+		InputStream fo = toFOConvertor
+				.performTransformation(new InputStreamResource(xhtml));
+		InputStream pdf = toPDFConvertor
+				.performTransformation(new InputStreamResource(fo));
+		return pdf;
+	}
+
+	public BookManager getBookManager() {
+		return bookManager;
+	}
+
+	public void setBookManager(BookManager bookManager) {
+		this.bookManager = bookManager;
+	}
+
+	public ChapterManager getChapterManager() {
+		return chapterManager;
+	}
+
+	public void setChapterManager(ChapterManager chapterManager) {
+		this.chapterManager = chapterManager;
+	}
+
+	public Convertor getToFOConvertor() {
+		return toFOConvertor;
+	}
+
+	public void setToFOConvertor(Convertor toFOConvertor) {
+		this.toFOConvertor = toFOConvertor;
+	}
+
+	public Convertor getToPDFConvertor() {
+		return toPDFConvertor;
+	}
+
+	public void setToPDFConvertor(Convertor toPDFConvertor) {
+		this.toPDFConvertor = toPDFConvertor;
+	}
+
+	public Convertor getToXHTMLConvertor() {
+		return toXHTMLConvertor;
+	}
+
+	public void setToXHTMLConvertor(Convertor toXHTMLConvertor) {
+		this.toXHTMLConvertor = toXHTMLConvertor;
+	}
+
+}

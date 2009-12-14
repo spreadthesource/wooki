16ba840d707e967fb0c50858241e2d0e1ade2b6d
Cordenier <christophe.cordenier@gmail.com>
2009-12-14 20:50:33 +0100

    Update last modified formatter to display a title with the real date

--- src/main/java/com/wooki/components/LastModifiedFormatter.java
+++ src/main/java/com/wooki/components/LastModifiedFormatter.java
@@ -1,6 +1,10 @@
 package com.wooki.components;
 
+import java.util.Date;
+
+import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.BeginRender;
 import org.apache.tapestry5.annotations.Parameter;
 
@@ -17,9 +21,18 @@ public class LastModifiedFormatter {
 	@Parameter(required = true, allowNull = false)
 	private long time;
 
+	@Parameter(value = "date", defaultPrefix = BindingConstants.LITERAL)
+	private String className;
+	
 	@BeginRender
 	public void displayDate(MarkupWriter writer) {
+		writer.element("abbr", "title", new Date(time), "class", className);
 		writer.write(LastActivityMessage.getActivityPeriod(time));
 	}
-	
+
+	@AfterRender
+	public void endAbbr(MarkupWriter writer) {
+		writer.end();
+	}
+
 }

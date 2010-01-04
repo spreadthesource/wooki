ad17fd26382f3948dc747ae9371b108bff5142d6
Cordenier <christophe.cordenier@gmail.com>
2010-01-04 17:29:45 +0100

    Format

--- src/main/java/com/wooki/services/WookiModule.java
+++ src/main/java/com/wooki/services/WookiModule.java
@@ -30,14 +30,10 @@ import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Match;
 import org.apache.tapestry5.ioc.annotations.SubModule;
 import org.apache.tapestry5.ioc.services.CoercionTuple;
-import org.apache.tapestry5.services.ApplicationInitializer;
-import org.apache.tapestry5.services.ApplicationInitializerFilter;
 import org.apache.tapestry5.services.AssetSource;
-import org.apache.tapestry5.services.Context;
 import org.apache.tapestry5.services.Dispatcher;
 import org.apache.tapestry5.services.PageRenderRequestFilter;
 import org.apache.tapestry5.util.StringToEnumCoercion;
-import org.springframework.context.ApplicationContext;
 import org.springframework.security.userdetails.UserDetailsService;
 
 import com.wooki.ActivityType;
@@ -46,114 +42,92 @@ import com.wooki.services.internal.TapestryOverrideModule;
 import com.wooki.services.security.UserDetailsServiceImpl;
 
 @SubModule(TapestryOverrideModule.class)
-public class WookiModule<T>
-{
+public class WookiModule<T> {
 
-    /**
-     * Used to stored the last view page in session.
-     */
-    public static final String VIEW_REFERER = "tapestry-view.referer";
+	/**
+	 * Used to stored the last view page in session.
+	 */
+	public static final String VIEW_REFERER = "tapestry-view.referer";
 
-    /**
-     * Use to encrypt the user password
-     */
-    public static final String SALT = "wookiwooki";
+	/**
+	 * Use to encrypt the user password
+	 */
+	public static final String SALT = "wookiwooki";
 
-    public void contributeApplicationDefaults(MappedConfiguration<String, String> conf)
-    {
-	conf.add(SymbolConstants.SUPPORTED_LOCALES, "en");
-	conf.add(SymbolConstants.PRODUCTION_MODE, "false");
-	conf.add(SymbolConstants.APPLICATION_VERSION, "0.1");
-    }
+	public void contributeApplicationDefaults(MappedConfiguration<String, String> conf) {
+		conf.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+		conf.add(SymbolConstants.PRODUCTION_MODE, "false");
+		conf.add(SymbolConstants.APPLICATION_VERSION, "0.1");
+	}
 
-    /**
-     * Wooki Symbols default
-     */
-    public static void contributeFactoryDefaults(MappedConfiguration<String, String> configuration)
-    {
-	configuration.add(WookiSymbolsConstants.UNHANDLED_BROWSER_PAGE, "error/unhandledbrowser");
-    }
+	/**
+	 * Wooki Symbols default
+	 */
+	public static void contributeFactoryDefaults(MappedConfiguration<String, String> configuration) {
+		configuration.add(WookiSymbolsConstants.UNHANDLED_BROWSER_PAGE, "error/unhandledbrowser");
+	}
 
-    public static void bind(ServiceBinder binder)
-    {
-	binder.bind(StartupService.class, StartupServiceImpl.class).eagerLoad();
-	binder.bind(UserDetailsService.class, UserDetailsServiceImpl.class);
-	binder.bind(SecurityUrlSource.class, SecurityUrlSourceImpl.class);
-	binder.bind(WookiViewRefererFilter.class);
-    }
+	public static void bind(ServiceBinder binder) {
+		binder.bind(StartupService.class, StartupServiceImpl.class).eagerLoad();
+		binder.bind(UserDetailsService.class, UserDetailsServiceImpl.class);
+		binder.bind(SecurityUrlSource.class, SecurityUrlSourceImpl.class);
+		binder.bind(WookiViewRefererFilter.class);
+	}
 
-    /**
-     * Store the last view page in session.
-     */
-    public static void contributePageRenderRequestHandler(OrderedConfiguration<PageRenderRequestFilter> filters, WookiViewRefererFilter vrFilter)
-    {
-	filters.add("ViewRefererFilter", vrFilter);
-    }
+	/**
+	 * Store the last view page in session.
+	 */
+	public static void contributePageRenderRequestHandler(OrderedConfiguration<PageRenderRequestFilter> filters, WookiViewRefererFilter vrFilter) {
+		filters.add("ViewRefererFilter", vrFilter);
+	}
 
-    /**
-     * Add request that shouldn't generate a referer.
-     * 
-     * @param excludePattern
-     */
-    public static void contributeWookiViewRefererFilter(Configuration<String> excludePattern)
-    {
-	excludePattern.add("signin");
-	excludePattern.add("signup");
-    }
+	/**
+	 * Add request that shouldn't generate a referer.
+	 * 
+	 * @param excludePattern
+	 */
+	public static void contributeWookiViewRefererFilter(Configuration<String> excludePattern) {
+		excludePattern.add("signin");
+		excludePattern.add("signup");
+		excludePattern.add(".*edit.*");
+	}
 
-    /**
-     * Add coercion tuple for paramter types...
-     * 
-     * @param configuration
-     */
-    public static void contributeTypeCoercer(Configuration<CoercionTuple> configuration)
-    {
-	CoercionTuple<String, ActivityType> tuple = new CoercionTuple<String, ActivityType>(String.class, ActivityType.class, StringToEnumCoercion.create(ActivityType.class));
-	configuration.add(tuple);
-    }
+	/**
+	 * Add coercion tuple for paramter types...
+	 * 
+	 * @param configuration
+	 */
+	public static void contributeTypeCoercer(Configuration<CoercionTuple> configuration) {
+		CoercionTuple<String, ActivityType> tuple = new CoercionTuple<String, ActivityType>(String.class, ActivityType.class, StringToEnumCoercion
+				.create(ActivityType.class));
+		configuration.add(tuple);
+	}
 
-    public void contributeApplicationInitializer(OrderedConfiguration<ApplicationInitializerFilter> configuration, final ApplicationContext springContext)
-    {
-	ApplicationInitializerFilter filter = new ApplicationInitializerFilter()
-	{
-	    public void initializeApplication(Context context, ApplicationInitializer initializer)
-	    {
-		// initializer.initializeApplication(context);
-	    }
+	/**
+	 * Add jQuery in no conflict mode to default JavaScript Stack
+	 * 
+	 * @param receiver
+	 * @throws NoSuchMethodException
+	 * @throws SecurityException
+	 */
+	@SuppressWarnings("unchecked")
+	@Match("ClientInfrastructure")
+	public static void adviseClientInfrastructure(MethodAdviceReceiver receiver, final AssetSource source) throws SecurityException, NoSuchMethodException {
+
+		MethodAdvice advice = new MethodAdvice() {
+			public void advise(Invocation invocation) {
+				invocation.proceed();
+				List<Asset> jsStack = (List<Asset>) invocation.getResult();
+				jsStack.add(source.getClasspathAsset("context:static/js/jquery-1.3.2.min.js"));
+				jsStack.add(source.getClasspathAsset("context:static/js/jquery.noconflict.js"));
+				jsStack.add(source.getClasspathAsset("context:static/js/wooki.js"));
+			}
+		};
+
+		receiver.adviseMethod(receiver.getInterface().getMethod("getJavascriptStack"), advice);
 	};
 
-	configuration.add("WookiContextInitialization", filter);
-    }
-
-    /**
-     * Add jQuery in no conflict mode to default JavaScript Stack
-     * 
-     * @param receiver
-     * @throws NoSuchMethodException
-     * @throws SecurityException
-     */
-    @SuppressWarnings("unchecked")
-    @Match("ClientInfrastructure")
-    public static void adviseClientInfrastructure(MethodAdviceReceiver receiver, final AssetSource source) throws SecurityException, NoSuchMethodException
-    {
-
-	MethodAdvice advice = new MethodAdvice()
-	{
-	    public void advise(Invocation invocation)
-	    {
-		invocation.proceed();
-		List<Asset> jsStack = (List<Asset>) invocation.getResult();
-		jsStack.add(source.getClasspathAsset("context:static/js/jquery-1.3.2.min.js"));
-		jsStack.add(source.getClasspathAsset("context:static/js/jquery.noconflict.js"));
-		jsStack.add(source.getClasspathAsset("context:static/js/wooki.js"));
-	    }
-	};
-
-	receiver.adviseMethod(receiver.getInterface().getMethod("getJavascriptStack"), advice);
-    };
-
-    public static void contributeMasterDispatcher(OrderedConfiguration<Dispatcher> configuration)
-    {
-	configuration.addInstance("UnhandledBrowserDispatcher", UnhandledBrowserDispatcher.class, "before:PageRender", "after:ComponentEvent");
-    }
+	public static void contributeMasterDispatcher(OrderedConfiguration<Dispatcher> configuration) {
+		configuration.addInstance("UnhandledBrowserDispatcher", UnhandledBrowserDispatcher.class, "before:PageRender", "after:ComponentEvent");
+	}
 }

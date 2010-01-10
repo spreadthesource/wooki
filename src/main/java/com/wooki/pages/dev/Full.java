d20b46f044274d1296e9d2a770a0699e263eed0b
Robin Komiwes <odisseus@gmail.com>
2010-01-10 23:53:26 +0100

    added full page under dev/. This page contains forms styles. Also improved lot of classes with tiny but very cool improvements

---
+++ src/main/java/com/wooki/pages/dev/Full.java
@@ -0,0 +1,37 @@
+//
+// Copyright 2009 Robin Komiwes, Bruno Verachten, Christophe Cordenier
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package com.wooki.pages.dev;
+
+import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.annotations.Symbol;
+
+public class Full {
+	@Inject
+	@Symbol(SymbolConstants.PRODUCTION_MODE)
+	private boolean productionMode;
+
+	@OnEvent(value = EventConstants.ACTIVATE)
+	private Object redirect() {
+		if (productionMode)
+			return com.wooki.pages.Index.class;
+		
+		return null;
+	}
+}

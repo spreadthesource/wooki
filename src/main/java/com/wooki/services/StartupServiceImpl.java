393d8c596d2f18c3dfd3c56450c04e2d8de204d5
Robin Komiwes <odisseus@gmail.com>
2010-01-18 23:09:35 +0100

    new startup service content

--- src/main/java/com/wooki/services/StartupServiceImpl.java
+++ src/main/java/com/wooki/services/StartupServiceImpl.java
@@ -37,22 +37,13 @@ public class StartupServiceImpl implements StartupService {
 	public StartupServiceImpl(ApplicationContext applicationContext, @Inject @Symbol(SymbolConstants.PRODUCTION_MODE) boolean productionMode)
 			throws UserAlreadyException, AuthorizationException {
 		// enabled headless mode
-		System.setProperty("java.awt.headless", "true"); 
-		
+		System.setProperty("java.awt.headless", "true");
 
 		if (!productionMode) {
 			WookiSecurityContext securityCtx = (WookiSecurityContext) applicationContext.getBean("wookiSecurityContext");
 			BookManager bookManager = (BookManager) applicationContext.getBean("bookManager");
 			ChapterManager chapterManager = (ChapterManager) applicationContext.getBean("chapterManager");
 			UserManager userManager = (UserManager) applicationContext.getBean("userManager");
-
-			// Add author to the book
-			User john = new User();
-			john.setEmail("john.doe@gmail.com");
-			john.setUsername("john");
-			john.setPassword("password");
-			john.setFullname("John Doe");
-			userManager.addUser(john);
 
 			User ccordenier = new User();
 			ccordenier.setEmail("christophe@gmail.com");
@@ -74,51 +65,59 @@ public class StartupServiceImpl implements StartupService {
 			robink.setPassword("password");
 			robink.setFullname("Robin K.");
 			userManager.addUser(robink);
-
-			securityCtx.log(john);
-
+			
+			securityCtx.log(robink);			
+			
 			// Create books
-			Book productBook = bookManager.create("Tapestry 5 : When development meets Art");
-			Book cacheBook = bookManager.create("My Cache Product Book");
-
-			// Create new chapters and modify its content
-			Chapter chapterOne = bookManager.addChapter(productBook, "Requirements");
-			chapterManager.updateAndPublishContent(chapterOne.getId(), "<p>You will need ...</p>");
-
-			// Add robin to author's list
-			try {
-				bookManager.addAuthor(productBook, "robink");
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-			securityCtx.log(robink);
-			Chapter chapterTwo = bookManager.addChapter(productBook, "Installation");
-
-			chapterManager.updateAndPublishContent(chapterTwo.getId(), "<p>First you have to set environment variables...</p>");
+			Book bookOfWooki = bookManager.create("The book of Wooki");
 
 			// publish Abstract
-			Chapter bookAbstract = chapterManager.listChaptersInfo(productBook.getId()).get(0);
+			Chapter bookAbstract = chapterManager.listChaptersInfo(bookOfWooki.getId()).get(0);
 			chapterManager
 					.updateAndPublishContent(
 							bookAbstract.getId(),
-							"<p>Apache Tapestry is an open-source framework for creating dynamic, robust, highly scalable web applications in Java. Tapestry complements and builds upon the standard Java Servlet API, and so it works in any servlet container or application server.</p><p>Tapestry divides a web application into a set of pages, each constructed from components. This provides a consistent structure, allowing the Tapestry framework to assume responsibility for key concerns such as URL construction and dispatch, persistent state storage on the client or on the server, user input validation, localization/internationalization, and exception reporting. Developing Tapestry applications involves creating HTML templates using plain HTML, and combining the templates with small amounts of Java code. In Tapestry, you create your application in terms of objects, and the methods and properties of those objects -- and specifically not in terms of URLs and query parameters. Tapestry brings true object oriented development to Java web applications.</p>");
-
-			securityCtx.log(john);
-			Publication published = chapterManager.getLastPublishedPublication(bookAbstract.getId());
-
+							"<p>What would you need if you had to write something and share it with people? We think you would be looking for Wooki : a publish platform offering the possibility to have direct feedback on what you have written.</p>");
+			
+			// Create new chapters and modify its content
+			Chapter chapterOne = bookManager.addChapter(bookOfWooki, "Collaborative document publishing");
 			chapterManager
-					.addComment(
-							published.getId(),
-							"Yes it's true !!! Yes it's true !!! Yes it's true !!!Yes it's true !!!Yes it's true !!!Yes it's true !!!Yes it's true !!!Yes it's true !!!Yes it's true !!! Yes it's true !!!Yes it's true !!! Yes it's true !!! Yes it's true !!!Yes it's true !!!",
-							"b10");
-			chapterManager
-					.addComment(
-							published.getId(),
-							"I agree but ... :) I agree but ... :) I agree but ... :) I agree but ... :) v I agree but ... :) I agree but ... :) I agree but ... :) I agree but ... :)I agree but ... :) I agree but ... :) I agree but ... :) I agree but ... :) I agree but ... :)",
-							"b10");
+					.updateAndPublishContent(
+							chapterOne.getId(),
+							"<p>The will to make Wooki came from a constatation. We noticed that the documentation we write every day at work did not get the expected result.</p>"
+									+ "<p>We used to produce lots of documents on multiples formats: Word, PDF, Powerpoint and of course on our intranet knowledge base.</p>"
+									+ "<p>Each format used to have its inconvenient: intranet knowledge base wasn't enough corporate, Word and PDF contents weren't indexed by our internal crawlers.</p>"
+									+ "<p>And the worst was that we almost never had  feedback on them. Why? Because we didn't proposed an easy way to collaborate with people</p>"
+									+ "<p>Wooki's goal is to offer a solution to all theses problems. Of course, Wooki is not finished, but we hope it will encounter a big future.</p>");
+
+			// Add robin to author's list
+			try {
+				bookManager.addAuthor(bookOfWooki, "ccordenier");
+				bookManager.addAuthor(bookOfWooki, "bverachten");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			
+			
+			Chapter chapterTwo = bookManager.addChapter(bookOfWooki, "Open source contribution");
+
+			chapterManager.updateAndPublishContent(chapterTwo.getId(), "<p>We are working since many months (...years?) on the web framework Tapestry 5. We always wanted to contribute in some way to it.</p>" +
+					"<p>Doing a project like Wooki was perfect to show the possibilities of this framework: technologies integration, ease of development. </p>");
+
+
+
+			Chapter chapterTree = bookManager.addChapter(bookOfWooki, "Get started");
+
+			chapterManager.updateAndPublishContent(chapterTree.getId(), "<h3>Try the demo</h3>" +
+					"<p>If you are reading that, you must already know that we host a demo website. You can freely create accounts and try Wooki there. </p>" +
+					"<h3>Build Wooki directly from the source code</h3>" +
+					"<p>Considering that you have already manipulated Tapestry 5 and maven, you can build the application directly from the source code.</p>" +
+					"<p>Go on Github, download the sources and run jetty executing this command:</p>" +
+					"<pre>" +
+					"mvn jetty:run" +
+					"</pre>" +
+					"<p>Launch your browser and that's it!</p>");
+
 		}
 	}
-
 }

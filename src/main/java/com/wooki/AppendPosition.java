c4c7d88eb5e3a24d6263880ecbb307214c54915e
Cordenier <christophe.cordenier@gmail.com>
2010-02-11 08:32:10 +0100

    Add mssing enum

---
+++ src/main/java/com/wooki/AppendPosition.java
@@ -0,0 +1,13 @@
+package com.wooki;
+
+/**
+ * Defines the positions where data must be append in the updated zone.
+ * 
+ * @author ccordenier
+ * 
+ */
+public enum AppendPosition {
+
+	TOP, BOTTOM;
+
+}

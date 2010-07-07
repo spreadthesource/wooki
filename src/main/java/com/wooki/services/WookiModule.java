000e6891cc896cd48283a67f87d3fff010416abb
ccordenier <christophe.cordenier@gmail.com>
2010-07-08 00:13:44 +0200

    clean useless hibernate file and fix unit test

--- src/main/java/com/wooki/services/WookiModule.java
+++ src/main/java/com/wooki/services/WookiModule.java
@@ -60,6 +60,7 @@ import org.apache.tapestry5.upload.services.MultipartDecoder;
 import org.apache.tapestry5.util.StringToEnumCoercion;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.spreadthesource.tapestry.spring.hibernate.SpringHibernateModule;
 import com.wooki.AppendPosition;
 import com.wooki.WookiSymbolsConstants;
 import com.wooki.core.services.CoreModule;
@@ -87,7 +88,7 @@ import com.wooki.services.security.spring.SecurityUrlSourceImpl;
 
 @SubModule(
 { TapestryOverrideModule.class, DataModule.class, ActivityModule.class, FeedModule.class,
-        CoreModule.class })
+        CoreModule.class, SpringHibernateModule.class })
 public class WookiModule<T>
 {
 
